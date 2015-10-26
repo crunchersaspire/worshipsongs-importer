072972b34fa5ff9d9bcb78d18eb13533cf4c015f
pitchumani <pitchu.rpm@gmail.com>
2015-10-26 16:20:05 +0800

    #13 Made some changes in SongTest class

--- src/test/java/org/worshipsongs/importer/SongTest.java
+++ src/test/java/org/worshipsongs/importer/SongTest.java
@@ -1,9 +1,11 @@
 package org.worshipsongs.importer;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,7 +15,7 @@ import static org.junit.Assert.*;
 public class SongTest
 {
     private SongParser parser = new SongParser();
-    String lyrics = "[V1]\n" +
+    private String lyrics = "[V1]\n" +
             "Lord I lift Your name on high\n" +
             "Lord I love to sing Your praises\n" +
             "[O1]\n" +
@@ -30,9 +32,9 @@ public class SongTest
             "From the grave to the sky\n" +
             "Lord I lift Your name on high";
 
-    String searchLyrics = "lord i lift your name on high lord i love to sing your praises im so glad youre in my life im so glad you came to save us you came from heaven to earth to show the way from the earth to the cross my debts to pay from the cross to the grave from the grave to the sky lord i lift your name on high";
+    private String searchLyrics = "lord i lift your name on high lord i love to sing your praises im so glad youre in my life im so glad you came to save us you came from heaven to earth to show the way from the earth to the cross my debts to pay from the cross to the grave from the grave to the sky lord i lift your name on high";
 
-    String xmlLyrics = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><song version=\"1.0\"><lyrics><verse label=\"1\" type=\"v\"><![CDATA[Lord I lift Your name on high\n" +
+    private String xmlLyrics = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><song version=\"1.0\"><lyrics><verse label=\"1\" type=\"v\"><![CDATA[Lord I lift Your name on high\n" +
             "Lord I love to sing Your praises]]></verse><verse label=\"1\" type=\"o\"><![CDATA[I’m so glad You're in my life\n" +
             "I’m so glad You came to save us]]></verse><verse label=\"1\" type=\"c\"><![CDATA[You came from heaven to earth\n" +
             "To show the way]]></verse><verse label=\"2\" type=\"o\"><![CDATA[From the earth to the cross,\n" +
@@ -41,20 +43,22 @@ public class SongTest
             "Lord I lift Your name on high]]></verse></lyrics></song>";
     Song song = new Song();
     Song song1 = new Song();
+    private ClassLoader classLoader;
 
     @Before
     public void setUp() throws IOException
     {
+        classLoader = getClass().getClassLoader();
         song.setTitle("Lord I lift Your Name");
         song.setAlternateTitle("Lord I lift Your Name");
-//        song.setAuthor("Unknown");
         song.setVerseOrder("V1 O1 C1 O2 O3");
-//        song.setSongBook("Foo");
         song.setLyrics(lyrics);
         song.setXmlLyrics(xmlLyrics);
-        song.setSearchTitle((song.getTitle()+"@"+song.getAlternateTitle()).toLowerCase());
+        song.setSearchTitle((song.getTitle() + "@" + song.getAlternateTitle()).toLowerCase());
         song.setSearchLyrics(searchLyrics);
-//        song1 = parser.parseSong("song.txt");
+        String input = IOUtils.toString(classLoader.getResourceAsStream("song.txt"));
+        List list = parser.parseSong(input);
+        song1 = (Song) list.get(0);
     }
 
     @Test
